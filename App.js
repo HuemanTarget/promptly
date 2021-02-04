958a78809656e59d2eefb8ecdf57996b407a5ff7
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-04 11:59:56 -0800

    create rounded button and logic to change background color on press

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import RoundedButton from "./RoundedButton";
 
 export default function App() {
+  const [color, setColor] = useState("black");
+
+  const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: color }]}>
       <Text>Open up App.js to start working on your app!</Text>
 
       <RoundedButton
         text="Next"
         textColor="white"
         backgroundColor="teal"
-        onPress={() => console.log("Pressed Button")}
+        onPress={() => setColor(randomRgb())}
       />
     </View>
   );
