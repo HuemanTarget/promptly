cfab5b8781363f688c7fe7a38f67e4cd27c7a723
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-04 11:49:41 -0800

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import RoundedButton from "./RoundedButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <RoundedButton
+        text="Next"
+        textColor="white"
+        backgroundColor="teal"
+        onPress={() => console.log("Pressed Button")}
+      />
     </View>
   );
 }
@@ -14,8 +20,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15,
   },
 });
